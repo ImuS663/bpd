793d8df1d974a187e7a26ce412f32ba4ef935966
Maksym Nedvyha <imus663@gmail.com>
2024-09-24 15:36:44 +0300

    feat: implement `ProgressBar` interface for `pterm.ProgressPrinter`

---
+++ pkg/writer/ptermpbar.go
@@ -0,0 +1,24 @@
+package writer
+
+import (
+	"github.com/pterm/pterm"
+)
+
+type PTermProgressBar struct {
+	pb *pterm.ProgressbarPrinter
+}
+
+func NewPTermProgressBar(name string, total int64) ProgressBar {
+	pb, _ := pterm.DefaultProgressbar.WithTotal(int(total)).WithTitle(name).WithShowCount(false).Start()
+
+	return &PTermProgressBar{pb: pb}
+}
+
+func (pb *PTermProgressBar) Add(n int) {
+	pb.pb.Add(int(n))
+}
+
+func (pb *PTermProgressBar) Close() {
+	pb.pb.WithCurrent(0)
+	pb.pb.Stop()
+}
