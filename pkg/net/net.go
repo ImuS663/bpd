a4402331d58789213df8472eb89ea02d5968838e
Maksym Nedvyha <imus663@gmail.com>
2024-09-24 15:21:29 +0300

    feat: add `InitReader` func

--- pkg/net/net.go
+++ pkg/net/net.go
@@ -1,8 +1,34 @@
 package net
 
-import "net/url"
+import (
+	"io"
+	"net/http"
+	"net/url"
+)
 
 func ValidateURL(urlForValidate string) bool {
 	_, err := url.ParseRequestURI(urlForValidate)
 	return err == nil
+}
+
+func InitReader(url string, headers map[string]string) (io.ReadCloser, int64, error) {
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	setHeaders(req, headers)
+
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, 0, err
+	}
+
+	return resp.Body, resp.ContentLength, nil
+}
+
+func setHeaders(req *http.Request, headers map[string]string) {
+	for key, value := range headers {
+		req.Header.Set(key, value)
+	}
 }
