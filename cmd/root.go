1b5075ad51f475b73db6332ed663bda9358afb0b
Maksym Nedvyha <imus663@gmail.com>
2024-09-25 14:29:22 +0300

    feat: implement parsing and downloading files

--- cmd/root.go
+++ cmd/root.go
@@ -1,17 +1,24 @@
 package cmd
 
 import (
+	"io"
 	"os"
+	"path/filepath"
 
+	"github.com/ImuS663/bpd/pkg/net"
+	"github.com/ImuS663/bpd/pkg/parser"
+	"github.com/ImuS663/bpd/pkg/pbar"
+	"github.com/ImuS663/bpd/pkg/writer"
+	"github.com/pterm/pterm"
 	"github.com/spf13/cobra"
 )
 
 var rootCmd = &cobra.Command{
 	Use:     "bpd [url...]",
 	Short:   "BPD is a CLI tool for downloading files from websites by Xpath.",
-	Args:    cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Args:    cobra.MatchAll(cobra.MinimumNArgs(1), cobra.OnlyValidArgs),
 	Example: "bpd https://example.com/p1 https://example.com/p2 -x '//*[@id=\"example\"]/div[1]' -H 'header1=value1' -H 'header2=value2'",
-	Run:     func(cmd *cobra.Command, args []string) {},
+	Run:     run,
 }
 
 var (
@@ -33,4 +40,69 @@ func init() {
 	rootCmd.Flags().StringToStringVarP(&headers, "header", "H", nil, "Request header")
 
 	rootCmd.MarkFlagRequired("xpath")
+}
+
+func run(cmd *cobra.Command, args []string) {
+	urls := make([]string, 0)
+	fulesUrls := make([]string, 0)
+
+	for _, url := range args {
+		if !net.ValidateURL(url) {
+			pterm.Info.Printf("Invalid URL: %s\n", url)
+		} else {
+			urls = append(urls, url)
+		}
+	}
+
+	if len(urls) == 0 {
+		pterm.Error.Println("No valid URLs found")
+		os.Exit(1)
+	}
+
+	parser := parser.NewParser(xpath)
+
+	for _, url := range urls {
+		result, err := parser.ParseFileURL(url)
+		if err != nil {
+			pterm.Error.Println(err)
+			continue
+		}
+
+		fulesUrls = append(fulesUrls, result)
+	}
+
+	if len(fulesUrls) == 0 {
+		pterm.Error.Println("No files found")
+		os.Exit(1)
+	}
+
+	for _, url := range fulesUrls {
+		reader, count, err := net.InitReader(url, headers)
+		if err != nil {
+			pterm.Error.Println(err)
+			continue
+		}
+		defer reader.Close()
+
+		fileName := filepath.Base(url)
+		pbar := pbar.NewPTermProgressBar(fileName, count)
+
+		filePath := filepath.Join(outDir, fileName)
+		file, err := os.OpenFile(filePath, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			pterm.Error.Println(err)
+			continue
+		}
+
+		writer := writer.NewProgressWriter(file, pbar)
+		defer writer.Close()
+
+		_, err = io.Copy(writer, reader)
+		if err != nil {
+			pterm.Error.Println(err)
+			continue
+		}
+
+		pterm.Success.Printf("File '%s' downloaded successfully\n", fileName)
+	}
 }
